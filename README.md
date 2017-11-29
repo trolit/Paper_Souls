5e1fb0b93b788a85879c9a59f4a0e098a309c8af
Idziku <iddziku@gmail.com>
2017-11-29 23:12:31 +0100

    Dodanie kart postaci i opisu

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Paper_Souls
-Paper Souls
+###########################################################################
+
+
+# Paper Souls
+## Projekt w stylu Dark Soulsów w konsoli.
+
+
+###########################################################################
+* Data rozpoczęcia: 27.11.17
+* Wykorzystany język: C#
+* Wykonanie w: Console Application, Visual Studio 2017 
--- paper_souls/paper_souls/Program.cs
+++ paper_souls/paper_souls/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace paper_souls
 {
@@ -37,8 +33,18 @@ namespace paper_souls
 
         public int Wybor_Bohatera()
         {
-            Console.WriteLine("Witaj!");
-            Console.WriteLine("Wybierz swojego bohatera i pokonaj przeciwników, którzy staną na twojej drodze!");
+            Console.WriteLine("------------------------->Witaj w grze Paper Souls!<-------------------------");
+            Console.Write("Pewnie końcówka 'Souls', trochę już zdradza o co chodzi w tej grze, jeśli jednak");
+            Console.Write("nie masz nadal zielonego pojęcia(nie martw się), już wyjaśniamy. Jest to gra typu ");
+            Console.Write("walka z przeciwnikami ale turowa, i ... tekstowa. Od Ciebie zależeć będzie życie ");
+            Console.Write("bohatera, ponieważ to Ty go poprowadzisz do boju. Na drodze spotkasz różnorakich wrogów. ");
+            Console.Write(" Aby pokonać swojego przeciwnika będziesz musiał wykazać się planowaniem kilku kroków do przodu. ");
+            Console.Write("Życie, mana (a w przypadku innych klas - inne atrybuty) wymuszą na Tobie ");
+            Console.Write("dobór odpowiedniej akcji do odpowiedniej sytuacji - jeśli się pomylisz - Twój ");
+            Console.Write("bohater zginie! Pamiętaj, że przeciwników może być więcej, zatem ostrożnie zużywaj surowce! ");
+            Console.Write("nie pozostaje Ci nic, tylko wybrać klasę i sprostać wyzwaniom jakie na Ciebie czekają! ");
+
+            Console.WriteLine("\n\nWybierz swojego bohatera i pokonaj przeciwników, którzy staną na twojej drodze!");
             int x;
             for (x = 0; x < 2;)
             {
@@ -47,7 +53,10 @@ namespace paper_souls
                 Console.WriteLine("3. Mag");
                 Console.WriteLine("4. Wzmacniacz");
                 Console.WriteLine("5. Templariusz");
-                Console.WriteLine("6. Druid\n");
+                Console.WriteLine("6. Druid");
+                Console.WriteLine("7. Snajper");
+                Console.WriteLine("8. Kusznik");
+                Console.WriteLine("9. Włócznik okrągłegu stołu");
                 Console.Write("Wybieram: ");
                 try
                 {
@@ -56,13 +65,14 @@ namespace paper_souls
                 catch
                 {
                     Console.WriteLine("\nPodczas działania programu wystąpił błąd!");
-                    Console.WriteLine("Podano błędne dane na wejściu wybierz_bohatera\n");
+                    Console.WriteLine("treść błędu: Podano błędne dane na wejściu wybierz_bohatera\n");
                     return 0;
                 }
 
                 if (wybierz_bohatera == 1)
                 {
-                    Console.WriteLine("___________________________________________\n");
+                    Console.WriteLine("_______________________________________________________" +
+                        "____\n");
                     Console.WriteLine("Horx 'Szczodry' - Paladyn(Człowiek)");
                     Console.WriteLine("\nGłówne cechy: \n");
                     Console.WriteLine("+ zdolność zwiększenia obrażen dzięki modlitwie");
@@ -73,13 +83,33 @@ namespace paper_souls
                     Console.WriteLine("Siła: 7");
                     Console.WriteLine("Zręczność: 2");
                     Console.WriteLine("Inteligencja: 6");
-                    Console.WriteLine("Żywotność(HP): 142");
+                    Console.WriteLine("Żywotność(HP): 160");
                     Console.WriteLine("Mana(MP): 100");
-                    Console.WriteLine("___________________________________________");
+                    Console.WriteLine("___________________________________________________________");
+                }
+                else if (wybierz_bohatera == 2)
+                {
+                    Console.WriteLine("___________________________________________________________\n");
+                    Console.WriteLine("Drakom 'Tarczownik' - Wojownik(Smok)");
+                    Console.WriteLine("\nGłówne cechy: \n");
+                    Console.WriteLine("+ duża wytrzymałość organizmu");
+                    Console.WriteLine("+ duża odporność na ciosy");
+                    Console.WriteLine("+ zdolność zionięcia ogniem");
+                    Console.WriteLine("+ umiejętność szarży z doskokiem");
+                    Console.WriteLine("+ ze względu na masywną broń, Drakom musi odpoczywać");
+                    Console.WriteLine("\nKarta postaci: \n");
+                    Console.WriteLine("Poziom: 4");
+                    Console.WriteLine("Siła: 12");
+                    Console.WriteLine("Zręczność: 1");
+                    Console.WriteLine("Inteligencja: 2");
+                    Console.WriteLine("Żywotność(HP): 200");
+                    Console.WriteLine("Mana(MP): 20");
+                    Console.WriteLine("Kondycja(CP): 115");
+                    Console.WriteLine("___________________________________________________________");
                 }
                 else if (wybierz_bohatera == 3)
                 {
-                    Console.WriteLine("___________________________________________\n");
+                    Console.WriteLine("___________________________________________________________\n");
                     Console.WriteLine("Harval 'Grzesznik' - Mag(Żaba)");
                     Console.WriteLine("\nGłówne cechy: \n");
                     Console.WriteLine("+ dużo czarów bitewnych");
@@ -92,7 +122,116 @@ namespace paper_souls
                     Console.WriteLine("Inteligencja: 9");
                     Console.WriteLine("Żywotność(HP): 110");
                     Console.WriteLine("Mana(MP): 125");
-                    Console.WriteLine("___________________________________________");
+                    Console.WriteLine("___________________________________________________________");
+                }
+                else if (wybierz_bohatera == 4)
+                {
+                    Console.WriteLine("___________________________________________________________\n");
+                    Console.WriteLine("Paseur 'Straszny' - Wzmacniacz(Rekin)");
+                    Console.WriteLine("\nGłówne cechy: \n");
+                    Console.WriteLine("+ potrafi wzmacniać topór zwiększając obrażenia");
+                    Console.WriteLine("+ może uniknąć następnego ataku przeciwnika");
+                    Console.WriteLine("+ potrafi stworzyć małą miksturę leczenia");
+                    Console.WriteLine("\nKarta postaci: \n");
+                    Console.WriteLine("Poziom: 4");
+                    Console.WriteLine("Siła: 3");
+                    Console.WriteLine("Zręczność: 6");
+                    Console.WriteLine("Inteligencja: 6");
+                    Console.WriteLine("Żywotność(HP): 92");
+                    Console.WriteLine("Mana(MP): 90");
+                    Console.WriteLine("Max.poziom wzmocnienia: 3");
+                    Console.WriteLine("___________________________________________________________");
+                }
+                else if (wybierz_bohatera == 5)
+                {
+                    Console.WriteLine("___________________________________________________________\n");
+                    Console.WriteLine("Marc 'Bezlitosny' - Templariusz(Człowiek)");
+                    Console.WriteLine("\nGłówne cechy: \n");
+                    Console.WriteLine("+ potrafi wyczuć poziom życia wroga");
+                    Console.WriteLine("+ potrafi szybko wykończyć wroga(poniżej pewnego progu życia)");
+                    Console.WriteLine("+ posiada zdolność riposty wielką tarczą pokrytą kolcami");
+                    Console.WriteLine("\nKarta postaci: \n");
+                    Console.WriteLine("Poziom: 3");
+                    Console.WriteLine("Siła: 10");
+                    Console.WriteLine("Zręczność: 1");
+                    Console.WriteLine("Inteligencja: 2");
+                    Console.WriteLine("Żywotność(HP): 129");
+                    Console.WriteLine("Mana(MP): 94");
+                    Console.WriteLine("Poziom kondycji(CP): 100");
+                    Console.WriteLine("___________________________________________________________");
+                }
+                else if (wybierz_bohatera == 6)
+                {
+                    Console.WriteLine("___________________________________________________________\n");
+                    Console.WriteLine("Malfurion 'Sędzia' - Druid(Człowiek)");
+                    Console.WriteLine("\nGłówne cechy: \n");
+                    Console.WriteLine("+ potrafi przekstrzałcić się w postać wilkołaka");
+                    Console.WriteLine("+ pazury pozwalają mu wywołać krwawienie u przeciwnika");
+                    Console.WriteLine("+ dysponuje zaklęciami ziemi");
+                    Console.WriteLine("\nKarta postaci: \n");
+                    Console.WriteLine("Poziom: 5");
+                    Console.WriteLine("Siła: 6");
+                    Console.WriteLine("Zręczność: 3");
+                    Console.WriteLine("Inteligencja: 6");
+                    Console.WriteLine("Żywotność(HP): 100");
+                    Console.WriteLine("Mana(MP): 110");
+                    Console.WriteLine("___________________________________________________________");
+                }
+                else if (wybierz_bohatera == 7)
+                {
+                    Console.WriteLine("___________________________________________________________\n");
+                    Console.WriteLine("Zajcev 'Sokole Oko' - Snajper(Duch)");
+                    Console.WriteLine("\nGłówne cechy: \n");
+                    Console.WriteLine("+ potrafi zniknąć z oczu przeciwnika");
+                    Console.WriteLine("+ potrafi celnie oddać strzał w głowę");
+                    Console.WriteLine("+ zna podstawowe zasady opatrywania ran");
+                    Console.WriteLine("+ jego broń, Dragunov jest na amunicję");
+                    Console.WriteLine("\nKarta postaci: \n");
+                    Console.WriteLine("Poziom: 6");
+                    Console.WriteLine("Siła: 6");
+                    Console.WriteLine("Zręczność: 5");
+                    Console.WriteLine("Inteligencja: 7");
+                    Console.WriteLine("Żywotność(HP): 100");
+                    Console.WriteLine("Amunicja(AP): 20");
+                    Console.WriteLine("___________________________________________________________");
+                }
+                else if (wybierz_bohatera == 8)
+                {
+                    Console.WriteLine("___________________________________________________________\n");
+                    Console.WriteLine("Bartolomeo 'Ćwierćfuntówka' - Kusznik(Umarły)");
+                    Console.WriteLine("\nGłówne cechy: \n");
+                    Console.WriteLine("+ korzysta z kuszy oblężniczej");
+                    Console.WriteLine("+ potrafi naciągnąć na kuszę jednocześnie 2 bełty");
+                    Console.WriteLine("+ nie zna magii");
+                    Console.WriteLine("+ nie potrafi się leczyć");
+                    Console.WriteLine("+ dysponuje bełtami z trucizną");
+                    Console.WriteLine("\nKarta postaci: \n");
+                    Console.WriteLine("Poziom: 6");
+                    Console.WriteLine("Siła: 10");
+                    Console.WriteLine("Zręczność: 3");
+                    Console.WriteLine("Inteligencja: 5");
+                    Console.WriteLine("Żywotność(HP): 300");
+                    Console.WriteLine("___________________________________________________________");
+                }
+                else if (wybierz_bohatera == 9)
+                {
+                    Console.WriteLine("___________________________________________________________\n");
+                    Console.WriteLine("Artur 'Sprawiedliwy' - Włócznik(Człowiek)");
+                    Console.WriteLine("\nGłówne cechy: \n");
+                    Console.WriteLine("+ potrafi szarżować na przeciwnika");
+                    Console.WriteLine("+ ma do pomocy gwardię łuczników");
+                    Console.WriteLine("+ jego broń nasycona jest magią(atak kosztuje manę i kondycję)");
+                    Console.WriteLine("+ umie wyciszyć umysł odzyskując zdrowie, kondycję i manę");
+                    Console.WriteLine("+ dręczy go masowampiryzm");
+                    Console.WriteLine("\nKarta postaci: \n");
+                    Console.WriteLine("Poziom: 6");
+                    Console.WriteLine("Siła: 7");
+                    Console.WriteLine("Zręczność: 7");
+                    Console.WriteLine("Inteligencja: 6");
+                    Console.WriteLine("Żywotność(HP): 135");
+                    Console.WriteLine("Poziom many(MP): 115");
+                    Console.WriteLine("Poziom kondycji(CP): 115");
+                    Console.WriteLine("___________________________________________________________");
                 }
 
                 Console.WriteLine("\nCzy jesteś pewien, że właśnie tego bohatera chcesz wybrać?");
@@ -105,7 +244,7 @@ namespace paper_souls
                 catch
                 {
                     Console.WriteLine("\nPodczas działania programu wystąpił błąd!");
-                    Console.WriteLine("Podano błędne dane na wejściu wybierz_bohatera_2\n");
+                    Console.WriteLine("treść błędu: Podano błędne dane na wejściu wybierz_bohatera_2\n");
                     return 0;
                 }
                 if (wybierz_bohatera == wybierz_bohatera_2)
@@ -236,7 +375,7 @@ namespace paper_souls
         public void Ulecz_sie(int mana, int zywotnosc)
         {
             this.mana = this.mana - 30;
-            this.zywotnosc = this.zywotnosc + 30;
+            this.zywotnosc = this.zywotnosc + return_health();
         }
 
         public void Modlitwa()
@@ -250,6 +389,13 @@ namespace paper_souls
             Random random = new Random();
             int atak = random.Next(6, 10);
             return atak;
+        }
+
+        public int return_health()
+        {
+            Random random = new Random();
+            int health = random.Next(30, 45);
+            return health;
         }
     }
 
@@ -315,7 +461,7 @@ namespace paper_souls
         {
             #region Tworzenie postaci do gry
             Postac wybor_bohatera = new Postac();
-            Paladyn paladyn1 = new Paladyn(100, 142, "Horx", 3, "Czlowiek", "Szczodry", 6, 7, 2);
+            Paladyn paladyn1 = new Paladyn(100, 160, "Horx", 3, "Czlowiek", "Szczodry", 6, 7, 2);
             Mag mag1 = new Mag(110, "Harval", 2, "Żaba", "Grzesznik", 125, 3, 9, 3, "Magia wody");
             OrkA ork1 = new OrkA(125, "Ork - Kusznik", 2, "orkowate", 2, 10);
             Szczur szczur1 = new Szczur(100, "Szczur", 2, "szczurowate", 2, 3);
@@ -334,17 +480,17 @@ namespace paper_souls
 
                 if (decyzja1 == 1)
                 {
-                    Console.WriteLine("Szczur zaczyna!");
+                    Console.WriteLine("Szczur jest szybszy i zaczyna!");
                     int obr_szczura = szczur1.Ugryzienie();
                     paladyn1.zywotnosc = paladyn1.zywotnosc - obr_szczura;
                     Console.WriteLine("Szczur ugryzł Cię zadając " + obr_szczura + " obrażeń!");
                     Console.WriteLine("Twoja kolej, co chcesz zrobić?");
                     Console.WriteLine("1. uderz młotem");
-                    Console.WriteLine("2. ulecz się(-30mana, +30hp) \n-> Stan many na chwilę obecną: " + paladyn1.mana);
-                    Console.WriteLine("3. modlitwa(+obrażenia na następną turę, +5hp)");
+                    Console.WriteLine("2. ulecz się(-30mana, +30-45hp)");
+                    Console.WriteLine("3. modlitwa(++obrażenia na następną turę, +5hp)");
                     Console.WriteLine("4. odpoczynek(+15mana, +6hp)");
-                    Console.WriteLine("Stan many: " + paladyn1.mana);
-                    Console.WriteLine("Stan życia: " + paladyn1.zywotnosc);
+                    Console.WriteLine("-> Stan many: " + paladyn1.mana);
+                    Console.WriteLine("-> Stan życia: " + paladyn1.zywotnosc);
 
                     int decyzja2 = Convert.ToInt32(Console.ReadLine());
                     if (decyzja2 == 1)
